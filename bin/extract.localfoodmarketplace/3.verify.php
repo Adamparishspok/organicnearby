914bb4b6183dafa68e9a5303652905108476d2f7
Atom Parish <hello@atomparish.com>
2023-02-16 14:37:38 -0800

    remove old files

--- bin/extract.localfoodmarketplace/3.verify.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$fp = fopen("output.csv", 'r');
-while ($line = fgetcsv($fp, 65535))
-		print_r($line);
--- bin/scrape.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-cd /var/www/sb4/sites/organicnearby.effortlessis.com/bin
-./scrape.php $1 $2
