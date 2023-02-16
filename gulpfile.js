d334b042b273de20ace3cedc81e36a8558a6e6fe
Atom Parish <hello@atomparish.com>
2023-02-16 15:55:25 -0800

    sadadd

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,53 @@
 // Load plugins
-const { src, dest, watch, parallel, series } = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const gulpautoprefixer = require('gulp-autoprefixer');
-const browsersync = require('browser-sync').create();
-const fileinclude = require('gulp-file-include');
-const useref = require('gulp-useref');
-const cached = require('gulp-cached');
-const gulpIf = require('gulp-if');
-const del = require('del');
-const npmDist = require('gulp-npm-dist');
-const postcss = require('gulp-postcss');
-const cssnano = require('cssnano');
-const autoprefixer = require('autoprefixer');
-const replace = require('gulp-replace');
-const gulpTerser = require('gulp-terser');
-const sourcemaps = require('gulp-sourcemaps');
-const uglify = require('gulp-uglify');
+const { src, dest, watch, parallel, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const gulpautoprefixer = require("gulp-autoprefixer");
+const browsersync = require("browser-sync").create();
+const fileinclude = require("gulp-file-include");
+const useref = require("gulp-useref");
+const cached = require("gulp-cached");
+const gulpIf = require("gulp-if");
+const del = require("del");
+const npmDist = require("gulp-npm-dist");
+const postcss = require("gulp-postcss");
+const cssnano = require("cssnano");
+const autoprefixer = require("autoprefixer");
+const replace = require("gulp-replace");
+const gulpTerser = require("gulp-terser");
+const sourcemaps = require("gulp-sourcemaps");
+const uglify = require("gulp-uglify");
 
 // Paths to project folders
 
 const paths = {
   base: {
-    base: './',
-    node: './node_modules',
+    base: "./",
+    node: "./node_modules",
   },
   src: {
-    basesrc: './src',
-    basesrcfiles: './src/**/*',
-    scss: './src/scss/**/*.scss',
-    css: './src/css',
-    js: './srcs/**/*.js',
-    vendorJs: './srcs/vendors/*.js',
-    html: './src/**/*.html',
-    images: './src/images/**/*',
-    fonts: './src/fonts/**/*',
-    assets: './src/**/*',
-    partials: '.src/**/*',
+    basesrc: "./src",
+    basesrcfiles: "./src/**/*",
+    scss: "./src/assets/scss/**/*.scss",
+    css: "./src/assets/css",
+    js: "./src/assets/js/**/*.js",
+    vendorJs: "./src/assets/js/vendors/*.js",
+    html: "./src/**/*.html",
+    images: "./src/assets/images/**/*",
+    fonts: "./src/assets/fonts/**/*",
+    assets: "./src/assets/**/*",
+    partials: ".src/partials/**/*",
   },
   temp: {
-    basetemp: './.temp',
+    basetemp: "./.temp",
   },
   dist: {
-    basedist: './dist',
-    js: './dists',
-    vendorJs: './dists/vendors',
-    images: './dist/images',
-    css: './dist/css',
-    fonts: './dist/fonts',
-    libs: './dist/libs',
+    basedist: "./dist",
+    js: "./dist/assets/js",
+    vendorJs: "./dist/assets/js/vendors",
+    images: "./dist/assets/images",
+    css: "./dist/assets/css",
+    fonts: "./dist/assets/fonts",
+    libs: "./dist/assets/libs",
   },
 };
 
@@ -55,9 +55,9 @@ const paths = {
 function scss(callback) {
   return src(paths.src.scss)
     .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass().on("error", sass.logError))
     .pipe(gulpautoprefixer())
-    .pipe(sourcemaps.write('.'))
+    .pipe(sourcemaps.write("."))
     .pipe(dest(paths.src.css))
     .pipe(browsersync.stream());
   callback();
@@ -83,21 +83,21 @@ function fonts(callback) {
 
 // HTML
 function html(callback) {
-  return src([paths.src.html, '!./src/**/*'])
+  return src([paths.src.html, "!./src/partials/**/*"])
     .pipe(
       fileinclude({
-        prefix: '@@',
-        basepath: '@file',
-      }),
+        prefix: "@@",
+        basepath: "@file",
+      })
     )
-    .pipe(replace(/src="(.{0,10})node_modules/g, 'src="$1libs'))
-    .pipe(replace(/href="(.{0,10})node_modules/g, 'href="$1libs'))
+    .pipe(replace(/src="(.{0,10})node_modules/g, 'src="$1assets/libs'))
+    .pipe(replace(/href="(.{0,10})node_modules/g, 'href="$1assets/libs'))
     .pipe(useref())
     .pipe(cached())
 
-    .pipe(gulpIf('*.css', postcss([autoprefixer(), cssnano()]))) // PostCSS plugins with cssnano
+    .pipe(gulpIf("*.css", postcss([autoprefixer(), cssnano()]))) // PostCSS plugins with cssnano
 
-    .pipe(gulpIf('*.js', gulpTerser()))
+    .pipe(gulpIf("*.js", gulpTerser()))
     .pipe(dest(paths.dist.basedist))
     .pipe(browsersync.stream());
   callback();
@@ -105,12 +105,12 @@ function html(callback) {
 
 // File include task for temp
 function fileincludeTask(callback) {
-  return src([paths.src.html, '!./src/**/*'])
+  return src([paths.src.html, "!./src/partials/**/*"])
     .pipe(
       fileinclude({
-        prefix: '@@',
-        basepath: '@file',
-      }),
+        prefix: "@@",
+        basepath: "@file",
+      })
     )
     .pipe(cached())
     .pipe(dest(paths.temp.basetemp));
@@ -156,7 +156,7 @@ function watchTask() {
   watch(paths.src.html, series(fileincludeTask, syncReload));
   watch(
     [paths.src.images, paths.src.fonts, paths.src.vendorJs],
-    series(images, fonts, vendorJs),
+    series(images, fonts, vendorJs)
   );
   watch([paths.src.scss], series(scss, syncReload));
 }
@@ -172,7 +172,7 @@ exports.build = series(
   fonts,
   vendorJs,
   copyLibs,
-  cleanTemp,
+  cleanTemp
 );
 
 // export tasks
